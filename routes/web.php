b30579cbbb037ecac6e849f8751c7c9ac9fde5bd
Augusto Goncalves Cardone <acardone@artit.com.br>
2019-05-07 13:50:22 -0300

    Projeto com login funcionando sem validação de e-mail

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Route::resource('contato', 'ContatoController');
 Route::resource('produtos', 'ProdutosController');
 Route::post('/produtos/busca', 'ProdutosController@busca');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
